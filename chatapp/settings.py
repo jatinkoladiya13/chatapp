2efc9f0866555a5ea25c93ea5f2ceaec6a1890c1
jatin <jatinkoladiya130@gmail.com>
2025-01-15 16:10:39 +0530

    collectstatic all static files

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -124,7 +124,7 @@ USE_I18N = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS =[os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = BASE_DIR /"media"
 
