e85c85e75a787f941d8440aea4b7b3f54bfe75a5
jatin <jatinkoladiya130@gmail.com>
2025-01-15 14:17:25 +0530

    solve static in settings.py

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -123,7 +123,7 @@ USE_I18N = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')],
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static'),
 
 MEDIA_ROOT = BASE_DIR /"media"
