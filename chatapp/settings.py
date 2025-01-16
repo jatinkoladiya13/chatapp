3bc1cea50bcbf5566d4986e625d825f49706a470
jatin <jatinkoladiya130@gmail.com>
2025-01-16 15:18:44 +0530

    change in settings.py

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -127,12 +127,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS =[os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_ROOT = BASE_DIR /"media"
 
