70cf5fbe018c6a31a4e5fea0a439c83e9efa3846
jatin <jatinkoladiya130@gmail.com>
2025-01-16 15:30:19 +0530

    change in build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,13 @@
 echo "Python version:"
 python3.12 --version
+
+pip install setuptools
+pip install -r requirements.txt
+
 echo "BUILD START"
-python3.12 -m pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate
+python manage.py tailwind install  
 python3.12 manage.py collectstatic --noinput 
+python manage.py tailwind start 
 echo "BUILD END"
