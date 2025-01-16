8cba38326bf27a9d8d452210b8ecc85fdbfa6698
jatin <jatinkoladiya130@gmail.com>
2025-01-16 14:29:40 +0530

    initial commit

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 echo "Python version:"
-python3.11 --version
+python3.9 --version
 echo "BUILD START"
-python3.11 -m pip install -r requirements.txt
-python3.11 manage.py collectstatic --noinput 
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput 
 echo "BUILD END"
