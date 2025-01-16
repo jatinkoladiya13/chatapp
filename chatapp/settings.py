1ff60887ac78fcceeef31caa5228cfa0520ecc6a
jatin <jatinkoladiya130@gmail.com>
2025-01-16 15:45:13 +0530

    Normalize line endings

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -124,7 +124,7 @@ USE_I18N = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS =[os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS =[BASE_DIR / "static"]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
