c7ce2f9ba264a96659f480ebcbd5bca70614d33e
jatin <jatinkoladiya130@gmail.com>
2025-01-16 15:03:09 +0530

    change env in  manage.py

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 import dotenv
 
 def main():
-    dotenv.load_dotenv()
+    dotenv.read_dotenv()
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatapp.settings')
     try:
