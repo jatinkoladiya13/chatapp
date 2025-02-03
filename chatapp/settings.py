16beb0728c359f419b0e540f714332ce72984ef9
jatin <jatinkoladiya130@gmail.com>
2025-02-03 14:51:21 +0530

    all good

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ie3kh()0+&2hy(@@c)hg3c-^!k%e31#8ys1g+(46w05$dr2$**
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -45,7 +45,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,9 +124,6 @@ USE_I18N = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS =[BASE_DIR / "static"]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_ROOT = BASE_DIR /"media"
 
